eef2703ae98e42806beace2bc53216b3e37c4c0f
William Goulart <goulart.w@gmail.com>
2021-08-19 23:25:14 -0300

    ensure LoginRouter returns 401 when invalid credentials ara provided

--- scr/presentation/routers/login-routers.spec.js
+++ scr/presentation/routers/login-routers.spec.js
@@ -69,4 +69,16 @@ describe('Login Router', () => {
     expect(authUseCaseSpy.email).toBe(httpRequest.body.email)
     expect(authUseCaseSpy.password).toBe(httpRequest.body.password)
   })
+
+  test('Should return 401 whn invalid credenctial ara provided', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'invalid_email@email.com',
+        password: 'invalid_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(401)
+  })
 })
