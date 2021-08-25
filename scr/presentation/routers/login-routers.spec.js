29a9448cca872809ce54117a93f7c42e420501b6
William Goulart <goulart.w@gmail.com>
2021-08-25 12:02:22 -0300

    ensure LoginRouter returns 500 if and and invalid AuthUseCase is provider

--- scr/presentation/routers/login-routers.spec.js
+++ scr/presentation/routers/login-routers.spec.js
@@ -1,6 +1,7 @@
 
 const LoginRouter = require('./login-router')
 const MissingParamError = require('../helpers/missing-param-error')
+const UnauthorizedError = require('../helpers/unauthorized-error')
 
 const makeSut = () => {
   class AuthUseCaseSpy {
@@ -70,7 +71,7 @@ describe('Login Router', () => {
     expect(authUseCaseSpy.password).toBe(httpRequest.body.password)
   })
 
-  test('Should return 401 whn invalid credenctial ara provided', () => {
+  test('Should return 401 when invalid credenctial ara provided', () => {
     const { sut } = makeSut()
     const httpRequest = {
       body: {
@@ -80,5 +81,32 @@ describe('Login Router', () => {
     }
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(401)
+    expect(httpResponse.body).toEqual(new UnauthorizedError())
   })
+
+  test('Should return 500 if no AuthUseCase is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+  
+  })  
+
+  test('Should return 500 if no AuthUseCase has no auth method', () => {   
+    const sut = new LoginRouter({})
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+  
+  })    
 })
